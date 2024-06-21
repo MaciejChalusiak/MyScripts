8041da271c4912deea5826eced7596aacc184967
Maciej Chalusiak <maciejchalusiak@Maciejs-MacBook-Pro.local>
2024-06-21 23:47:35 +0200

    Klima, sterowanie razem z przyciskami do wlaczania

---
+++ klima/buttony.py
@@ -0,0 +1,60 @@
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BCM)
+
+button_pin_salon = 6
+button_pin_biuro = 5
+button_pin_sypialnia = 4
+
+GPIO.setup(button_pin_salon, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_pin_biuro, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_pin_sypialnia, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+main_folder_dir = r'/home/maciek/Desktop/klima'
+
+command = [
+    'python3',
+    f'{main_folder_dir}/sterowanie.py',
+    '--selected_device',
+]
+
+salon = 'first'
+biuro = 'first'
+sypialnia = 'first'
+
+
+while True:
+    if GPIO.input(button_pin_biuro) == GPIO.LOW:
+        print('Nacinieto biuro')
+        if biuro != 'first' and biuro.poll() is None:
+            print('biuro off')
+            biuro.kill()
+        else:
+            print('biuro on')
+            biuro = subprocess.Popen([*command, 'Biuro'])
+        time.sleep(1)
+
+    if GPIO.input(button_pin_sypialnia) == GPIO.LOW:
+        print('Nacinieto sypialnia')
+        if sypialnia != 'first' and sypialnia.poll() is None:
+            print('sypialnia off')
+            sypialnia.kill()
+        else:
+            print('sypialnia on')
+            sypialnia = subprocess.Popen([*command, 'Sypialnia'])
+        time.sleep(1)
+
+
+    if GPIO.input(button_pin_salon) == GPIO.LOW:
+        print('Nacinieto salon')
+        if salon != 'first' and salon.poll() is None:
+            print('salon off')
+            salon.kill()
+        else:
+            print('salon on')
+            salon = subprocess.Popen([*command, 'Salon'])
+        time.sleep(1)
+
+    time.sleep(0.1)
