045c0b6bba16ffa18c9bf6e57a344339d7985959
Maciej Chalusiak <maciejchalusiak@gmail.com>
2024-06-28 10:57:42 +0200

    Air conditioning optimalization for button.py

--- klima/buttony.py
+++ klima/buttony.py
@@ -14,45 +14,44 @@ GPIO.setup(button_pin_sypialnia, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 main_folder_dir = r'/home/maciek/Desktop/klima'
 
-command = [
+sterowanie_command = [
     'python3',
     f'{main_folder_dir}/sterowanie.py',
     '--selected_device',
 ]
 
-salon = 'first'
-biuro = 'first'
-sypialnia = 'first'
+oled_command = [
+    'python3',
+    f'{main_folder_dir}/oled.py',
+    '--text',
+]
+
+devices = {
+    'Salon': {'process': None},
+    'Biuro': {'process': None},
+    'Sypialnia': {'process': None},
+}
+
+
+def button_pressed(device):
+    print(f'Nacinieto {device}')
+    if devices[device]['process'] is not None and devices[device]['process'].poll() is None:
+        subprocess.Popen([*oled_command, f'{device} - OFF'])
+        print(f'{device} off')
+        devices[device]['process'].terminate()
+    else:
+        subprocess.Popen([*oled_command, f'{device} - ON'])
+        print(f'{device} on')
+        devices[device]['process'] = subprocess.Popen([*sterowanie_command, f'{device}'])
+    time.sleep(0.5)
 
 
 while True:
     if GPIO.input(button_pin_biuro) == GPIO.LOW:
-        print('Nacinieto biuro')
-        if biuro != 'first' and biuro.poll() is None:
-            print('biuro off')
-            biuro.terminate()
-        else:
-            print('biuro on')
-            biuro = subprocess.Popen([*command, 'Biuro'])
-        time.sleep(0.5)
+        button_pressed('Biuro')
 
     if GPIO.input(button_pin_sypialnia) == GPIO.LOW:
-        print('Nacinieto sypialnia')
-        if sypialnia != 'first' and sypialnia.poll() is None:
-            print('sypialnia off')
-            sypialnia.terminate()
-        else:
-            print('sypialnia on')
-            sypialnia = subprocess.Popen([*command, 'Sypialnia'])
-        time.sleep(0.5)
-
+        button_pressed('Sypialnia')
 
     if GPIO.input(button_pin_salon) == GPIO.LOW:
-        print('Nacinieto salon')
-        if salon != 'first' and salon.poll() is None:
-            print('salon off')
-            salon.terminate()
-        else:
-            print('salon on')
-            salon = subprocess.Popen([*command, 'Salon'])
-        time.sleep(0.5)
+        button_pressed('Salon')
